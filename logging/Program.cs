b9de5aba888319cf33f959be4078193af2ca234a
RushikeshNarkhedePatil <rushikeshnarkhede4@gmail.com>
2022-08-26 17:07:59 +0530

    first commit

---
+++ logging/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            logger.Info("App Started {arguments} {username}",args,"fullstackamigo");
+            try
+            {
+
+            }
+            catch(Exception e)
+            {
+                logger.Error(e, "this is unexpected! {args}", 2);
+            }
+            NLog.LogManager.Shutdown();
+
+        }
+    }
+}
